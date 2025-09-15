c667eaeb37150ea6d5e6292909e6c0a088abd9af
Sophie <soph1155@stud.kea.dk>
2025-09-15 14:29:46 +0200

    udkommenter script

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
-document.querySelector('#frmContact').addEventListener('submit', (e) => {
-    e.preventDefault();
+// document.querySelector('#frmContact').addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    const fullName = e.target.txtFullName.value;
-    const email = e.target.txtEmail.value;
-    const phoneNumber = e.target.txtPhoneNumber.value;
-    const comments = e.target.txtComments.value;
+//     const fullName = e.target.txtFullName.value;
+//     const email = e.target.txtEmail.value;
+//     const phoneNumber = e.target.txtPhoneNumber.value;
+//     const comments = e.target.txtComments.value;
 
-    const dialog = document.querySelector('#mdlContact')
+//     const dialog = document.querySelector('#mdlContact')
 
-    dialog.querySelector('#form-full-name').innerText = fullName;
-    dialog.querySelector('#form-email').innerText = email;
-    dialog.querySelector('#form-phone-number').innerText = phoneNumber;
-    dialog.querySelector('#form-comments').innerText = comments;
+//     dialog.querySelector('#form-full-name').innerText = fullName;
+//     dialog.querySelector('#form-email').innerText = email;
+//     dialog.querySelector('#form-phone-number').innerText = phoneNumber;
+//     dialog.querySelector('#form-comments').innerText = comments;
 
-    dialog.showModal();
+//     dialog.showModal();
 
-    e.target.reset();
+//     e.target.reset();
 
-    dialog.querySelector('button.close').addEventListener('click', (e) => {
-        e.preventDefault();
+//     dialog.querySelector('button.close').addEventListener('click', (e) => {
+//         e.preventDefault();
 
-        dialog.close();
-    })
-});
+//         dialog.close();
+//     })
+// });
